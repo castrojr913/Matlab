64fb171ab03b20781646b1c889d2549991971122
Jesús Castro <castrojr913@gmail.com>
2019-03-07 13:28:07 -0500

    Rename taller2.m to polos_ceros.m

--- Calculo Numerico/polos_ceros.m
+++ Calculo Numerico/polos_ceros.m
@@ -1,7 +1,7 @@
 %PUNTO 4 TALLER DE CONTROL 1
 clear
 syms s
-disp('Funcion Transferencia Ecuaci髇 3.a)')
+disp('Funcion Transferencia Ecuaci贸n 3.a)')
 g1=(5/s+2*s-6)/(2*s^2+4*s+1)
 [num,den]=numden(g1);
 v1=sym2poly(num);
@@ -9,7 +9,7 @@ v2=sym2poly(den);
 [z,p,k]=tf2zp(v1,v2) %polos,ceros y ganancia
 figure(1)
 pzmap(v1,v2)%grafica polos y ceros
-disp('Funcion Transferencia Ecuaci髇 3.b)')
+disp('Funcion Transferencia Ecuaci贸n 3.b)')
 g2=(10*s-8/s+1)/(5*s^3+3*s^2+s-2)
 [num,den]=numden(g2);
 v1=sym2poly(num);
@@ -17,7 +17,7 @@ v2=sym2poly(den);
 [z,p,k]=tf2zp(v1,v2) %polos,ceros y ganancia
 figure(2)
 pzmap(v1,v2)%grafica polos y ceros
-disp('Funcion Transferencia Ecuaci髇 3.c)')
+disp('Funcion Transferencia Ecuaci贸n 3.c)')
 g3=(6-6*s-2/s)/(6*s^2-12*s+1)
 [num,den]=numden(g3);
 v1=sym2poly(num);
@@ -25,7 +25,7 @@ v2=sym2poly(den);
 [z,p,k]=tf2zp(v1,v2) %polos,ceros y ganancia
 figure(3)
 pzmap(v1,v2)%grafica polos y ceros
-disp('Funcion Transferencia Ecuaci髇 3.d)')
+disp('Funcion Transferencia Ecuaci贸n 3.d)')
 g4=(7/s+28*s-32)/(4*s^2+1)
 [num,den]=numden(g4);
 v1=sym2poly(num);
